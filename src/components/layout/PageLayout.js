cea85d1bfbdeb32d15ac07df5da50f8e3eaaf8a0
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-08-05 14:36:33 +0200

    Fix text color of auth screen

--- src/components/layout/PageLayout.js
+++ src/components/layout/PageLayout.js
@@ -3,8 +3,12 @@ import React from "react";
 import CenteredLayout from "./CenteredLayout";
 import Footer from "../Footer";
 
-const PageLayout = ({ children }) => (
-  <div className="min-h-screen bg-gray-800 flex flex-col justify-between">
+const PageLayout = ({ children, className }) => (
+  <div
+    className={`min-h-screen bg-gray-800 flex flex-col justify-between ${
+      className ? className : ""
+    }`}
+  >
     <CenteredLayout>{children}</CenteredLayout>
     <CenteredLayout>
       <Footer />
